a48c5279d4065e67e52134234a8c9ab3c8eebb94
Erica Pietroniro <erica.piet306@gmail.com>
2021-01-19 23:29:57 -0500

    use perceptron learning on sample data, output plot of results. need to debug

--- Assignment1/question1.py
+++ Assignment1/question1.py
@@ -12,6 +12,8 @@ import numpy as np
 import os
 import pickle
 import pandas as pd
+import matplotlib.pyplot as plt
+from random import random
 
 #%% Load Data
 filename = os.path.abspath(
@@ -21,16 +23,75 @@ labels = data['labels'].astype(np.int)
 vectors = data['vectors']
 
 #put in dataframe
-training_df = pd.DataFrame(columns=["v_1", "v_2", "labels"])
-training_df.v_1 = [x for x in vectors[0]]
-training_df.v_2 = [x for x in vectors[1]]
+training_df = pd.DataFrame(columns=["f_1", "f_2", "labels", "outputs"])
+training_df.f_1 = [x for x in vectors[0]]
+training_df.f_2 = [x for x in vectors[1]]
 training_df.labels = labels
 
-#%% Perceptron
+#%% Definitions
 
-step_fun = lambda x: 0 if x < 0 else 1
+activation_fun = lambda x: 0 if x < 0 else 1
 
-training_input = training_df.sample(frac=1)
+input_fun = lambda x, w: activation_fun(
+    bias + x[0]*weights[0] + x[1]*weights[1])
 
-weights = np.random.randint(2, size=100)
+delta_w = lambda x, y_desired, y_output: gamma*x*(y_desired - y_output)
+
+
+def change_weight(df):
+    weights[0] += delta_w(df.f_1, df.labels, df.outputs)
+    weights[1] += delta_w(df.f_2, df.labels, df.outputs)
+
+
+def perceptron(df, weights, gamma, bias):
+    
+    #produce predicted output
+    df.outputs = training_df.apply(
+        lambda x: input_fun(
+            np.array([x.f_1, x.f_2]), weights), axis=1)
+    
+    # adjust the weights
+    df.apply(lambda x: change_weight(x), axis=1)
+    
+    return df, weights
+    
+    
+#%% Training
+
+
+gamma = 0.01
+bias = -1
+steps = range(300)
+fraction_correct_graph = []
+   
+# Run 10 times
+for i in range(10):
+
+    weights = [random(), random()]
+    fraction_correct_steps = [] 
+   
+    for ii in range(300):
+        epoch = training_df.sample(frac=1)
+        training_output, new_weights = perceptron(epoch, weights, gamma, bias)
+        
+        weights = new_weights
+        
+        # determine fraction of correct outputs
+        is_correct = training_output.apply(
+            lambda x: 1 if x.labels == x.outputs else 0, axis=1)
+        
+        fraction_correct_steps.append(is_correct.sum()/100)
+            
+        fraction_correct_graph.append(fraction_correct_steps)
+    
+    print(fraction_correct_steps[-1])
+
+
+#%% Plot
+
+for i in range(10):
+    plt.figure(i)
+    plt.scatter(steps, fraction_correct_graph[i])
+
+
 
